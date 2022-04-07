a873f638eecf65397879bc5d6ee46763e566e8d2
Gediminas Melinauskas <g.melinauskas0895@gmail.com>
2022-04-07 07:50:49 +0100

    fixed few bugs in the code

--- function.php
+++ function.php
@@ -1,13 +1,14 @@
 <?php
-function getbooks($pdo ,$books)
+
+function getbooks(array $books)
 {
     foreach ($books as $book) {
-        echo '<div class="content">'
-            . '<img src=' . $book['image'] . '>'
-            . '<h2>' . $book['auther'] . '</h2>'
-            . '<h2>' . $book['title'] . '</h2>'
-            . '<h3>' . $book['chapter'] . '</h3>'
-            . '<p>' . $book['description'] . '</p>'
+        echo  '<div class="content">'
+            . '<img src=' . $book["image"] . ' class="cover" alt="' . $book["title"] . ' books cover">'
+            . '<h2>' . $book["auther"] . '</h2>'
+            . '<h2>' . $book["title"] . '</h2>'
+            . '<h3>' . $book["chapter"] . '</h3>'
+            . '<p>' . $book["description"] . '</p>'
             . '</div>';
     }
 }
--- index.php
+++ index.php
@@ -1,20 +1,43 @@
 <?php
 require_once 'connect.php';
 require_once 'function.php';
+
+if (isset($_POST['create'])) {
+    $auther = $_POST['auther'];
+    $title = $_POST['title'];
+    $chapter = $_POST['chapter'];
+    $description = $_POST['description'];
+    $image = $_POST['image'];
+    echo $auther . ' ' . $title . ' ' . $chapter . ' ' . $description . ' ' . $image;
+
+    $sql = "INSERT INTO books (auther, title, chapter, description, image) VALUES ('$auther', '$title', '$chapter', '$description', '$image')";
+    $stmtinsert = connectToDB('booksDB')->prepare($sql);
+    $results = $stmtinsert->execute();
+
+    if ($results) {
+        echo ' we are in';
+
+
+    } else {
+        echo 'broken';
+    }
+}
 ?>
+
 <!doctype html>
 <html lang="en">
+
 <head>
-    <link rel="stylesheet" href="styles/normalize.css">
-    <link href="styles/style.css" rel="stylesheet">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Collection</title>
 </head>
+
 <body>
 <h1>BOOK COLLECTION</h1>
 <div class="container">
-<?php getbooks($pdo, $books); ?>
+<?php getbooks($books);?>
 </div>
+
 </body>
 </html>
