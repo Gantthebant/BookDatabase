1bfe07277860b715f97b9226ddf1fa38b9d7aca1
Gediminas Melinauskas <g.melinauskas0895@gmail.com>
2022-04-07 09:35:59 +0100

    got ride off some old code of testing

--- index.php
+++ index.php
@@ -1,32 +1,10 @@
 <?php
 require_once 'db.php';
 require_once 'function.php';
-
-if (isset($_POST['create'])) {
-    $auther = $_POST['auther'];
-    $title = $_POST['title'];
-    $chapter = $_POST['chapter'];
-    $description = $_POST['description'];
-    $image = $_POST['image'];
-    echo $auther . ' ' . $title . ' ' . $chapter . ' ' . $description . ' ' . $image;
-
-    $sql = "INSERT INTO books (auther, title, chapter, description, image) VALUES ('$auther', '$title', '$chapter', '$description', '$image')";
-    $stmtinsert = connectToDB('booksDB')->prepare($sql);
-    $results = $stmtinsert->execute();
-
-    if ($results) {
-        echo ' we are in';
-
-
-    } else {
-        echo 'broken';
-    }
-}
 ?>
 
 <!doctype html>
 <html lang="en">
-
 <head>
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/styles.css">
@@ -34,7 +12,6 @@ if (isset($_POST['create'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Collection</title>
 </head>
-
 <body>
 <h1>BOOK COLLECTION</h1>
 <div class="splash">
