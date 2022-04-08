9d13fcc9fd7438ec71e925fca4d63b7e5382cea1
Gediminas Melinauskas <g.melinauskas0895@gmail.com>
2022-04-08 11:27:20 +0100

    fixed testing

--- test/test.php
+++ test/test.php
@@ -23,8 +23,10 @@ class Test extends \PHPUnit\Framework\TestCase
 
     public function testGivenValidBookReturn()
     {
+        $expected = '<div class="content"><img src=images/lastempire.jpeg class="cover" alt="The Last Empire books cover"><div class="text"><h2>Brandon Sanderson</h2><hr class="top"><h2>The Last Empire</h2><h3>Mistborn</h3><hr class="bot"><p>The Final Empire, the first book in a trilogy by Brandon Sanderson, is a tale of a subjugated people known as the Skaa, and their fight for freedom against a seemingly invincible God known as the Lord Ruler.The Lord Ruler has ruled this world for a thous</p></div></div>';
         $records[] = array
         (
+
             'id' => 1,
             'auther' => 'Brandon Sanderson',
             'title' => 'The Last Empire',
@@ -35,7 +37,7 @@ class Test extends \PHPUnit\Framework\TestCase
 
         $result = getbooks($records);
 
-        $this->assertEquals($expected, $result);
+        $this->assertEquals($expected,$result);
     }
     
 }
